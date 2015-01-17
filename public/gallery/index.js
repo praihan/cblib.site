bdf1b500a32a49c320a7d70ed5e99d63ba81b3c5
prshreshtha <prshreshtha@yahoo.com>
2015-01-16 22:42:43 -0500

    Added gallery page and a slideshow to news page

--- public/gallery/index.js
+++ public/gallery/index.js
@@ -6,20 +6,19 @@
     $("#gallery").nanoGallery({
       kind: "flickr",
       userID: "129159678@N02", // colonelbylibrary@yahoo.ca
-      photoset: "72157649919607620",
       thumbnailWidth: 165,
       thumbnailHeight: 110,
       thumbnailHoverEffect: "labelSlideUp,borderLighter",
       thumbnailLazyLoad:true,
       viewerDisplayLogo:false,
-      paginationMaxLinesPerPage: 1,
+      paginationMaxLinesPerPage: 3,
       thumbnailLabel: {display: true, align: "center"},
       theme: "clean",
       i18n: { thumbnailImageDescription: "view photo", thumbnailAlbumDescription: "open album" }
     });
   });
 
-  // news via rss
+  // hide spinner
   $(window).load(function() {
     $("#ajax-spinner").hide();
   });
