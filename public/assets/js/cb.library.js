edc4aa3e608bbc9f397851a709489d8c2b00d852
prshreshtha <prshreshtha@yahoo.com>
2015-01-16 19:28:17 -0500

    3Konami5Me

--- public/assets/js/cb.library.js
+++ public/assets/js/cb.library.js
@@ -50,6 +50,18 @@
     });
   }
 
+  if (typeof String.prototype.repeat === "undefined") {
+    String.prototype.repeat = function(count) {
+      if (count < 1) return '';
+      var result = '', pattern = this.valueOf();
+      while (count > 1) {
+        if (count & 1) result += pattern;
+        count >>= 1, pattern += pattern;
+      }
+      return result + pattern;
+    };
+  }
+
   String.prototype.cbTruncate = function(n, useWordBoundary) {
     if (typeof useWordBoundary == "undefined") {
       useWordBoundary = true;
