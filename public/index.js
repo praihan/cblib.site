9a336f25b95fab536248fdc8ceb1ea017f62a5de
prshreshtha <prshreshtha@yahoo.com>
2015-01-18 01:03:24 -0500

    Finished news page, added images and quotes to required pages

--- public/index.js
+++ public/index.js
@@ -7,20 +7,22 @@
       var $e = $("#ajax-spinner");
       window.CB.Library.parseRSS(document.location.protocol + "//colonelbylibrary.wordpress.com/feed/?nocache=" + new Date().getTime(), function(data) {
         $.each(data.feed.entries, function(key, value) {
-          var $event = $("#upcoming-event-" + (key + 1));
-          var eventHref = window.CB.Library.REL_PATH + "/news/#upcoming-event-" + (key + 1);
-          var publishDate = new Date(value.publishedDate);
-          $event
-            .append($("<h3/>", {"class": "evt-date"}).html(window.CB.Library.getMonthString(publishDate.getMonth()) + " " + publishDate.getDate()))
-            .append($("<h2/>", {"class": "evt-title"})
-              .append($("<a/>", {href: eventHref}).html(value.title.cbTruncate(40))))
-            .append($("<p/>", {"class": "evt-content"}).html(value.contentSnippet + "..."))
-            .append($("<p/>")
-              .append($("<a/>", {"class": "btn btn-default evt-view-btn", href: eventHref, role: "button"}).html("Read More &raquo;")))
-          if (key + 1 != data.feed.entries.length) {
+          var $event = $("#news-" + (key + 1));
+          if ($event.length) {
+            var eventHref = window.CB.Library.REL_PATH + "/news/#recent-" + (key + 1);
+            var publishDate = new Date(value.publishedDate);
             $event
-              .append($("<br/>", {"class": "evt-break"}))
-              .append($("<hr/>", {"class": "evt-break"}));
+              .append($("<h3/>", {"class": "evt-date"}).html(window.CB.Library.getMonthString(publishDate.getMonth()) + " " + publishDate.getDate()))
+              .append($("<h2/>", {"class": "evt-title"})
+                .append($("<a/>", {href: eventHref}).html(value.title.cbTruncate(40))))
+              .append($("<p/>", {"class": "evt-content"}).html(value.contentSnippet + "..."))
+              .append($("<p/>")
+                .append($("<a/>", {"class": "btn btn-default evt-view-btn", href: eventHref, role: "button"}).html("Read More &raquo;")))
+            if (key + 1 != data.feed.entries.length) {
+              $event
+                .append($("<br/>", {"class": "evt-break"}))
+                .append($("<hr/>", {"class": "evt-break"}));
+            }
           }
         });
         $e.hide();
--- public/news/index.js
+++ public/news/index.js
@@ -6,16 +6,18 @@
     var $e = $("#ajax-spinner");
     window.CB.Library.parseRSS(document.location.protocol + "//colonelbylibrary.wordpress.com/feed/?nocache=" + new Date().getTime(), function(data) {
       $.each(data.feed.entries, function(key, value) {
-        var $event = $("#upcoming-event-" + (key + 1));
-        var publishDate = new Date(value.publishedDate);
-        $event
-          .append($("<h3/>", {"class": "evt-date"}).html(window.CB.Library.getMonthString(publishDate.getMonth()) + " " + publishDate.getDate()))
-          .append($("<h2/>", {"class": "evt-title"})
-            .append($("<a/>", {href: value.link}).html(value.title.cbTruncate(100))))
-          .append($("<div/>", {"style": "-moz-box-shadow: 0 0 3px #ccc;-webkit-box-shadow: 0 0 3px #ccc;box-shadow: 0 0 3px #ccc; border:15px solid transparent;"})
-            .append($("<p/>", {"class": "evt-content",}).html(value.content))
-            .append($("<p/>"))
-              .append($("<a/>", {"class": "btn btn-default evt-view-btn", href: value.link, role: "button"}).html("Read on WordPress&#8480; &raquo;")));
+        var $event = $("#recent-" + (key + 1));
+        if ($event.length) {
+          var publishDate = new Date(value.publishedDate);
+          $event
+            .append($("<h3/>", {"class": "evt-date"}).html(window.CB.Library.getMonthString(publishDate.getMonth()) + " " + publishDate.getDate() + ", " + publishDate.getFullYear()))
+            .append($("<h2/>", {"class": "evt-title"})
+              .append($("<a/>", {href: value.link}).html(value.title.cbTruncate(100))))
+            .append($("<div/>", {"style": "-moz-box-shadow: 0 0 3px #ccc;-webkit-box-shadow: 0 0 3px #ccc;box-shadow: 0 0 3px #ccc; border:15px solid transparent;"})
+              .append($("<p/>", {"class": "evt-content",}).html(value.content))
+              .append($("<p/>"))
+                .append($("<a/>", {"class": "btn btn-default evt-view-btn", href: value.link, role: "button"}).html("Read on <i>WordPress&#8480;</i> &raquo;")));
+        }
       });
       $e.hide();
     }, function() {
@@ -32,8 +34,8 @@
       thumbnailWidth: 165,
       thumbnailHeight: 110,
       thumbnailHoverEffect: "labelSlideUp,borderLighter",
-      thumbnailLazyLoad:true,
-      viewerDisplayLogo:false,
+      thumbnailLazyLoad: true,
+      viewerDisplayLogo: false,
       paginationMaxLinesPerPage: 1,
       thumbnailLabel: {display: true, align: "center"},
       theme: "clean",
